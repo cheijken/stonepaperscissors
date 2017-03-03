f382df1b99aa8d27cea5489b9fa4ea540c1f7623
extgdi <georgios.dimitriadis@ica.se>
2017-03-03 14:23:53 +0100

    actions and reducers

--- frontend/src/routes/Home/components/HomeView.js
+++ frontend/src/routes/Home/components/HomeView.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
 import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
+import { ping } from './HomeViewAction'
 
-export const HomeView = () => (
+const HomeView = (props) => (
   <div>
     <h4>Welcome!</h4>
+    <button onClick={props.pingBackend}>Ping!</button>
+    <p>Reply: {props.pingReply}</p>
     <img
       alt='This is a duck, because Redux!'
       className='duck'
@@ -12,4 +16,19 @@ export const HomeView = () => (
   </div>
 )
 
-export default HomeView
+HomeView.propTypes = {
+  pingReply: PropTypes.string,
+  pingBackend: PropTypes.func.isRequired
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    pingBackend : () => { dispatch(ping()) }
+})
+
+const mapStateToProps = (state) => ({
+  pingReply : state.pingReply
+})
+
+const HomeViewContainer = connect(mapStateToProps, mapDispatchToProps)(HomeView)
+
+export default HomeViewContainer
